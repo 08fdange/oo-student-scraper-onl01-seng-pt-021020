0e31b6fafa4b62d7d52588b63af60aaba7ca8a6b
Frank  D'Angelo <08fdange@gmail.com>
2020-03-18 03:53:08 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -11,7 +11,7 @@ class Student
 
   def self.create_from_collection(students_array)
     students_array.each do |studnt|
-      new_student = self.new(studnt)
+      self.new(studnt)
     end
   end
 
